b6708a245299f99597e86e6c0611b8365a0e749b
mataperezluis <mata.perez.luis@gmail.com>
2020-10-10 21:00:10 -0300

    mejoras

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -23,6 +23,8 @@ export class HeaderComponent implements OnInit {
   particles = [];
   fuenteMono1:any;
   sizeRaton = 0;
+  scala = 0;
+  imgOrnitorrincut:any;
   //st = 'Ornitorrin-cut, es un proyecto multidisciplinario, nacido  de la reunión de distintas  naturalezas en equilibrio, destinado a crear piezas y alimentos visuales de alta gama    S u i    g e n e r i s.';
   st = "Ornitorrincut:";
   st2="Confección de universos visuales";
@@ -42,26 +44,29 @@ export class HeaderComponent implements OnInit {
       {
         this.sizeRaton = s.windowWidth/30;
         this.fuenteMono1 = s.loadFont("assets/fonts/RobotoMono-Regular.ttf");
-
+        this.imgOrnitorrincut = s.loadImage('assets/logo.png');
+        this.scala=0.4;
 
       }
 
 
       s.setup = () => {
-        let canvas2 = s.createCanvas(s.windowWidth-20, s.windowHeight*4);
+        let canvas2 = s.createCanvas(s.windowWidth+50, s.windowHeight);
         canvas2.parent('sketch-holder');
         canvas2.position(0,0);
-        canvas2.style("z-index : -1");
-        s.background(10, 38, 53);
+        canvas2.style("z-index : 1");
+        s.background(5,25,36);
 
-        for(let i = 0; i<s.windowWidth/40; i++){
+        s.image(this.imgOrnitorrincut, s.windowWidth/4, 10);
+
+        for(let i = 0; i<s.windowWidth/60; i++){
             this.particles.push(new Particle(s));
         }
 
       };
 
       s.draw = () => {
-      s.background(10, 38, 53);
+      s.background(5,25,36);
       //s.fill('#ED225D');
       s.textStyle(s.ITALIC);
       s.textSize(s.windowWidth/28);
@@ -70,6 +75,10 @@ export class HeaderComponent implements OnInit {
       s.textSize(s.windowWidth/30);
       s.text(this.st2, (s.windowWidth/9)*3, s.windowHeight - s.windowHeight/2, s.windowWidth, s.windowHeight); // Text wraps within text box
 
+      s.imageMode(s.CENTER);
+      s.image(this.imgOrnitorrincut, s.windowWidth/2, s.windowHeight/4,this.scala*s.windowWidth, this.scala*this.imgOrnitorrincut.height*s.windowWidth/this.imgOrnitorrincut.width);
+
+      
       if(s.mouseY > s.windowHeight + 20 && s.mouseY < s.windowHeight*2 + 20)
       {
         s.triangle(s.mouseX - this.sizeRaton/2, s.mouseY + this.sizeRaton/2, s.mouseX + this.sizeRaton/2, s.mouseY + this.sizeRaton/2, s.mouseX, s.mouseY - this.sizeRaton/2);
